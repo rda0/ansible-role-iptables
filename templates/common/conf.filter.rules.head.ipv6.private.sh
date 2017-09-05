09a757a2265bf59a2993bae5cb1b09f2499f1cc1
Sven Mäder <maeder@phys.ethz.ch>
2017-09-05 18:24:52 +0200

    Add bridge capability to iptables

--- templates/common/conf.filter.rules.head.ipv6.private.sh
+++ templates/common/conf.filter.rules.head.ipv6.private.sh
@@ -12,11 +12,14 @@ for IF in ${IF_EXT}; do
   echo -e "-A FORWARD -m set --match-set private6 src -i ${IF} -j f-priv"
 done
 
+for IF in ${IF_BR_EXT}; do
+  echo -e "-A INPUT   -m set --match-set private6 src -m physdev --physdev-in ${IF} -j i-priv"
+  echo -e "-A FORWARD -m set --match-set private6 src -m physdev --physdev-in ${IF} -j f-priv"
+done
+
 echo -e "# Anything from this range gets dropped, except for our allowed networks"
 
-for IF in ${IF_EXT}; do
-  echo -e "-A i-priv  -m set ! --match-set private6-except src -j LOG ${LOG_OPTS} \"[ipt-i]${IF}-s-priv: \""
-  echo -e "-A i-priv  -m set ! --match-set private6-except src -j DROP"
-  echo -e "-A f-priv  -m set ! --match-set private6-except src -j LOG ${LOG_OPTS} \"[ipt-f]${IF}-s-priv: \""
-  echo -e "-A f-priv  -m set ! --match-set private6-except src -j DROP"
-done
+echo -e "-A i-priv  -m set ! --match-set private6-except src -j LOG ${LOG_OPTS} \"[ipt-i]any-s-priv: \""
+echo -e "-A i-priv  -m set ! --match-set private6-except src -j DROP"
+echo -e "-A f-priv  -m set ! --match-set private6-except src -j LOG ${LOG_OPTS} \"[ipt-f]any-s-priv: \""
+echo -e "-A f-priv  -m set ! --match-set private6-except src -j DROP"
--- templates/common/conf.filter.rules.tail.sh
+++ templates/common/conf.filter.rules.tail.sh
@@ -5,6 +5,11 @@
 echo -e "# All other connections are registered in syslog"
 #echo -e "-A INPUT   -j LOG ${LOG_OPTS} \"[ipt-i]: \""
 echo -e "-A OUTPUT  -j LOG ${LOG_OPTS} \"[ipt-o]: \""
+
+for IF in ${IF_BR}; do
+  echo -e "-A FORWARD -i ${IF} -o ${IF} -j ACCEPT"
+done
+
 echo -e "-A FORWARD -j LOG ${LOG_OPTS} \"[ipt-f]: \""
 echo -e "# And finally rejected (trusted)"
 echo -e "-A INPUT   -m set --match-set ${SET_TRU} src -j REJECT"
